4512173621ca499ec984a537aed779bff403f630
Shubhankit Saxena <shubhankit.saxena@raksan.in>
2018-05-06 14:37:56 +0530

    fixes the signup number validation

--- routes/create.js
+++ routes/create.js
@@ -5,7 +5,19 @@ var User = require('../User');
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 var Data = {};
+var _ = require('underscore');
+
+function phone (number){
+	if(number[0]== "+" &&number[1]== "9" &&number[2]== "1" ){
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
 router.use(function(req, resp, next){
+	var num = [];
 	var userData = req.body;
 	User.findOne({username: userData.username}, function(err, res){
 		if(err){
@@ -15,25 +27,34 @@ router.use(function(req, resp, next){
 			resp.send('User Exists');
 		}
 		else {
-			bcrypt.genSalt(10, function(err, salt) {
-		    bcrypt.hash(userData.password, salt, function(err, hash) {
-	        var num = [];
-	        var newData = new User({
-	        	username: userData.username,
-	        	roleId: "2",
-						password: hash,
-						contactNumber: num.concat(userData.contact)
-	        });
-	        Data = {
-		   			username:userData.username,
-		   			password:hash,
-		   			roleId:"2",
-		   			contactNumber:num.concat(userData.contact)
-		   		}
-	        newData.save();
-	        next();
-		    });
+			var nonValidPhone = _.find(num.concat(userData.contact), function(contact){
+				var val = phone(contact);
+				return val;
 			});
+			if(nonValidPhone){
+				resp.status(417).send('Invalid Number');
+			}
+			else {
+				bcrypt.genSalt(10, function(err, salt) {
+			    bcrypt.hash(userData.password, salt, function(err, hash) {
+		        var num = [];
+		        var newData = new User({
+		        	username: userData.username,
+		        	roleId: "2",
+							password: hash,
+							contactNumber: num.concat(userData.contact)
+		        });
+		        Data = {
+			   			username:userData.username,
+			   			password:hash,
+			   			roleId:"2",
+			   			contactNumber:num.concat(userData.contact)
+			   		}
+		        newData.save();
+		        next();
+			    });
+				});
+			}
 		}
 	})
 });
